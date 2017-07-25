9f27e60bfbb974a373096da079d982967afdde60
Micha Alt <mail@lean-stack.rocks>
2017-07-25 22:08:17 +0200

    Some TDD: create test, next code

--- tests/unit/model/todo.spec.ts
+++ tests/unit/model/todo.spec.ts
@@ -1,8 +1,14 @@
 describe('Model Todo', () => {
 
+    let todo: Todo;
+
+    beforeEach(() => {
+        todo = new Todo();
+    });
+
     it('is creatable', () => {
 
         expect(true).toBe(true);
-        
+
     });
 });

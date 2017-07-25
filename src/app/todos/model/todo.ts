a9c8caaa9ec5f7181d53f3361e759cbf699f5038
Micha Alt <mail@lean-stack.rocks>
2017-07-25 22:47:35 +0200

    A little TDD experience - but remember JS is dynamic. Tests are fullfilling without declared properties.

--- src/app/todos/model/todo.ts
+++ src/app/todos/model/todo.ts
@@ -1,2 +1,9 @@
 export class Todo {
+    public id: number;
+    public text: string;
+    public done: boolean;
+
+    constructor(text: string = '') {
+        this.text = text;
+    }
 }
--- tests/unit/model/todo.spec.ts
+++ tests/unit/model/todo.spec.ts
@@ -10,7 +10,35 @@ describe('Model Todo', () => {
 
     it('is creatable', () => {
 
-        expect(true).toBe(true);
+        expect(todo instanceof Todo).toBeTruthy();
 
+    });
+
+    it('has an id property', () => { 
+
+        todo.id = 17;
+        expect(todo.id).toEqual(17);
+
+    });
+
+    it('has a text property', () => { 
+
+        todo.text = 'Unit tests';
+        expect(todo.text).toEqual('Unit tests');
+
+    });
+
+    it('has a done property', () => { 
+
+        todo.done = true; 
+        expect(todo.done).toEqual(true);
+
+    });
+
+    it('can be created with initial text', () => {
+
+        const todo = new Todo('Unit Tests');
+        expect(todo.text).toEqual('Unit Tests');
+        
     });
 });

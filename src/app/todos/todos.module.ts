61f3dbc704c8a926909349f571b926a54c93a283
Micha Alt <mail@lean-stack.rocks>
2017-07-27 23:22:06 +0200

    Todo store service created. Test moved to integration folder.

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { StoreService } from './store.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [TodoItemComponent],
-  exports: [TodoItemComponent]
+  exports: [TodoItemComponent],
+  providers: [StoreService]
 })
 export class TodosModule { }
